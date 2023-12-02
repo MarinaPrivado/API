cc674d7af89eb7dcc692d2bb1bf17ff26805460d
viniciussilvabarros05 <viniciussilvabarros05@hotmail.com>
2023-12-02 16:50:44 -0300

    feat: CRUD finalizado

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 //rota para criar um item
 app.post("/item", async (req, res) => {
   const dados = req.body;
-  await prisma.item.create({
+  await prisma.user.create({
     data: {
       nome: dados.nome,
     },
@@ -25,10 +25,62 @@ app.post("/item", async (req, res) => {
 
 //rota para listar todos os usuários
 app.get("/users", async (req, res) => {
+  try{
+    const data  = await prisma.user.findMany()
+    return res.json(data)
+  }catch(error){
+    return res.sendStatus(404)
+  }
+
 });
 
 //rota para buscar um usuário pelo nome
 app.get("/user/:nome", async (req, res) => {
+  const {nome} = req.params 
+  try{
+    const data  = await prisma.user.findMany({
+      where: {
+        nome : nome
+      }
+    })
+    if(data) {
+      return res.json(data)
+    }
+  }catch(error){
+    return res.sendStatus(404)
+  }
+});
+
+app.put("/user/update", async (req, res) => {
+  const {id, nome} = req.body 
+  try{
+    await prisma.user.updateMany({
+      where: {
+        id 
+      },
+      data: {
+        nome: nome
+      }
+    })
+    res.send("Nome alterado com sucesso!")
+  }catch(error){
+    return res.sendStatus(404)
+  }
+});
+
+app.delete("/user/delete", async (req, res) => {
+  const {id} = req.body 
+  try{
+     await prisma.user.deleteMany({
+      where: {
+        id
+      }
+    })
+
+    return res.send("Usuário deletado")
+  }catch(error){
+    return res.sendStatus(404)
+  }
 });
 
 // Inicie o servidor na porta especificada
